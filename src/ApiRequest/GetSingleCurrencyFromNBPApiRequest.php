8c9002669b744098833e5c8867a8d568c724bd80
pawel.dziura <pawel.dziura@dgsoftware.pl>
2023-02-19 19:30:49 +0100

    getSingleCurrencyFromNBPApiRequest class, and its getSingleCurrency function, which is returning CurrencyDTO. added urlApiNBP to env, and added urlApiNBP which is connected to urlApiNBP from env to default binding. Its added call setUrlApiNBP with required attribute on getSingleCurrencyFromNBPApiRequest automatically to set urlApiNBP on autoinjection.

---
+++ src/ApiRequest/GetSingleCurrencyFromNBPApiRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\ApiRequest;
+
+
+use App\DTO\CurrencyDTO;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Service\Attribute\Required;
+
+/**
+ *
+ */
+class GetSingleCurrencyFromNBPApiRequest
+{
+    /** @var string */
+    private $urlApiNBP;
+    private HttpClientInterface $client;
+
+    /**
+     * @param HttpClientInterface $client
+     */
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getSingleCurrency(string $currencyCode): CurrencyDTO
+    {
+        $response = $this->client->request(
+            'GET',
+            $this->urlApiNBP . "/exchangerates/rates/A/$currencyCode/"
+        );
+
+        $contentArray = $response->toArray();
+        $newestCurrencyRateDate = null;
+        $newestCurrencyRate = null;
+        foreach ($contentArray['rates'] as $currencyRate) {
+            $currentEffectiveDate = new \DateTime($currencyRate['effectiveDate']);
+            if (!$newestCurrencyRateDate && !$newestCurrencyRate) {
+                $newestCurrencyRateDate = $currentEffectiveDate;
+                $newestCurrencyRate = $currencyRate['mid'];
+            } else {
+                if ($newestCurrencyRate < $currentEffectiveDate) {
+                    $newestCurrencyRateDate = $currentEffectiveDate;
+                    $newestCurrencyRate = $currencyRate['mid'];
+                }
+            }
+        }
+        $currencyDTO = new CurrencyDTO($contentArray['currency'], $contentArray['code'], $newestCurrencyRate);
+        return $currencyDTO;
+    }
+
+    #[Required]
+    public function setUrlApiNBP(string $urlApiNBP): void
+    {
+        $this->urlApiNBP = $urlApiNBP;
+    }
+}
